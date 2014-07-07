a936c228a7a0116194cc6610c514a4ac2dd64ae5
Makoto Chiba <hypermkt@paperboy.co.jp>
2014-07-07 13:56:28 +0900

    7.3の所を作業中

--- rails/sample_app/config/routes.rb
+++ rails/sample_app/config/routes.rb
@@ -3,6 +3,7 @@ SampleApp::Application.routes.draw do
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
