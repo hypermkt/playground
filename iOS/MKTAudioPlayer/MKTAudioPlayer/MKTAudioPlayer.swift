e72a6046cafa93afea0fcdee481720a257548f89
Makoto Chiba <hypermkt@paperboy.co.jp>
2014-07-08 19:59:13 +0900

    とりあえず必要そうなメソッドのインターフェースだけ準備

--- iOS/MKTAudioPlayer/MKTAudioPlayer/MKTAudioPlayer.swift
+++ iOS/MKTAudioPlayer/MKTAudioPlayer/MKTAudioPlayer.swift
@@ -11,15 +11,51 @@ import AVFoundation
 
 class MKTAudioPlayer {
 
-    var _bubbleView: UIView;
+    var audioFilePath: NSString
+    var parentViewController: UIViewController
+    var isVisible : Bool
+    var isBubbleViewVisible: Bool
+    var _bubbleView: UIView
     
     init() {
-        self._bubbleView = UIView();
+        self.audioFilePath = "" as NSString
+        self.parentViewController = UIViewController()
+        self.isVisible = false // TOOD: とりあえず初期化エラー防止のため。要変更
+        self.isBubbleViewVisible = false; // TOOD: とりあえず初期化エラー防止のため。要変更
+        self._bubbleView = UIView()
+    }
+    
+    func initWithAudioFilePath(audioFilePath: NSString, parentViewController:UIViewController) -> MKTAudioPlayer {
+        return self;
+    }
+
+    func initWithAudioFilePath(width: CGFloat, height: CGFloat) -> MKTAudioPlayer {
+        return self;
+    }
+    
+    func play() {
+        
+    }
+
+    func pause() {
+        
+    }
+
+    func dismiss() {
+        
+    }
+
+    func showAnimated(animated:Bool) {
+        
+    }
+
+    func hideAnimated(aanimated:Bool) {
+        
     }
 
     func setIsBubbleViewVisible(isBubbleViewVisible:Bool)
     {
-        var _isBubbleViewVisible = isBubbleViewVisible;
-        self._bubbleView.hidden = !isBubbleViewVisible;
+        var _isBubbleViewVisible = isBubbleViewVisible
+        self._bubbleView.hidden = !isBubbleViewVisible
     }
 }
