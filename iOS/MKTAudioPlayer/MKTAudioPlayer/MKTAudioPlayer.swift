3aa78683fdb465061c8d3e64000ab65689a35e51
Makoto Chiba <hypermkt@paperboy.co.jp>
2014-07-09 20:47:25 +0900

    途中コミット

--- iOS/MKTAudioPlayer/MKTAudioPlayer/MKTAudioPlayer.swift
+++ iOS/MKTAudioPlayer/MKTAudioPlayer/MKTAudioPlayer.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class MKTAudioPlayer: UIView {
+class MKTAudioPlayer :UIView {
 
     var _playerHeight: Float;
     var _playerWidth: Float;
@@ -23,8 +23,13 @@ class MKTAudioPlayer: UIView {
 
     var _audioPlayer: AVAudioPlayer
     var _playPauseButton: UIButton
-    
+
     init() {
+        
+        self._playerHeight = 0.0
+        self._playerWidth = 0.0
+        self._inset = 0.0
+
         self.audioFilePath = "" as NSString
         self.parentViewController = UIViewController()
         self.isVisible = false // TOOD: とりあえず初期化エラー防止のため。要変更
@@ -34,13 +39,114 @@ class MKTAudioPlayer: UIView {
         
         self._audioPlayer = AVAudioPlayer()
         self._playPauseButton = UIButton()
+        
     }
-    
+
     func initWithAudioFilePath(audioFilePath: NSString, parentViewController:UIViewController) -> MKTAudioPlayer {
+        var frame: CGRect = CGRectMake(0, 0, parentViewController.view.bounds.size.width, 75.0)
+        
+        // selfへの代入はどうする？
+        self.initWithAudioFilePath(frame.size.width, height: frame.size.height);
+        
+        frame.origin.y = parentViewController.view.bounds.size.height;
+        self.frame = frame;
+        parentViewController.view.addSubview(self);
+        parentViewController.view.bringSubviewToFront(self);
+        
         return self;
     }
 
     func initWithAudioFilePath(width: CGFloat, height: CGFloat) -> MKTAudioPlayer {
+        if (height == 0) height = 75.0;
+        
+        CGRect frame = CGRectMake(0, 0, width, height);
+        
+        self = [super initWithFrame:frame];
+        
+        if (self) {
+            
+            _audioFilePath = audioFilePath;
+            NSAssert(audioFilePath != nil, @"Audio file path cannot be nil");
+            
+            _playerHeight = frame.size.height;
+            _playerWidth = frame.size.width;
+            _inset = 15;
+            
+            NSURL *url = [NSURL fileURLWithPath:audioFilePath];
+            NSError *error = nil;
+            _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+            _audioPlayer.volume = 0.5;
+            _audioPlayer.delegate = self;
+            NSAssert(error == nil, @"Audio file not found");
+            
+            [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+            [[AVAudioSession sharedInstance] setActive: YES error:nil];
+            
+            long totalPlaybackTime = _audioPlayer.duration;
+            int tHours = (int)(totalPlaybackTime / 3600);
+            int tMins = (int)((totalPlaybackTime/60) - tHours*60);
+            int tSecs = (int)(totalPlaybackTime % 60 );
+            _durationString = (tHours > 0) ? [NSString stringWithFormat:@"%i:%02d:%02d", tHours, tMins, tSecs ] : [NSString stringWithFormat:@"%02d:%02d", tMins, tSecs];
+            
+            _timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(onTimer:) userInfo:nil repeats:YES];
+            
+            self.clipsToBounds = NO;
+            self.autoresizesSubviews = YES;
+            self.backgroundColor = [UIColor clearColor];
+            
+            UIView *backgroundView = [[UIView alloc] initWithFrame:self.bounds];
+            backgroundView.backgroundColor = RGB(0xe8e8e8);
+            backgroundView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+            [self addSubview:backgroundView];
+            
+            UIImageView *playerBgImageView = [[UIImageView alloc] initWithImage:[[UIImage imageNamed:@"player_player_bg"] stretchableImageWithLeftCapWidth:5 topCapHeight:5]];
+            playerBgImageView.frame = CGRectMake(_inset, _inset, _playerWidth - _inset * 2, _playerHeight - _inset * 2);
+            playerBgImageView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+            [self addSubview:playerBgImageView];
+            
+            _playPauseButton = [UIButton buttonWithType:UIButtonTypeCustom];
+            _playPauseButton.autoresizesSubviews = YES;
+            _playPauseButton.imageView.contentMode = UIViewContentModeScaleToFill;
+            [_playPauseButton setImage:[UIImage imageNamed:@"player_play"] forState:UIControlStateNormal];
+            [_playPauseButton setFrame:CGRectMake(_inset, _inset, _playerHeight - 2 * _inset, _playerHeight - 2 * _inset)];
+            [_playPauseButton addTarget:self action:@selector(playOrPause) forControlEvents:UIControlEventTouchUpInside];
+            [self addSubview:_playPauseButton];
+            
+            float originY = _playerHeight / 2.0 - 34 / 2;
+            float originX = _playPauseButton.frame.origin.x + _playPauseButton.frame.size.width + _inset;
+            CGRect sliderFrame = CGRectMake(originX, originY, frame.size.width - originX - _inset * 2, 34);
+            _slider = [[UISlider alloc] initWithFrame:sliderFrame];
+            _slider.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+            if ( ! IS_IOS7_OR_HIGHER ) {
+                [_slider setMaximumTrackImage:[[UIImage imageNamed:@"player_progress_bg"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 10, 0, 10)]  forState:UIControlStateNormal];
+                [_slider setMinimumTrackImage:[[UIImage imageNamed:@"player_progress_blue"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 10, 0, 10)]  forState:UIControlStateNormal];
+            }
+            [_slider setThumbImage:[UIImage imageNamed:@"player_circle"] forState:UIControlStateNormal];
+            _slider.minimumValue = 0.0;
+            _slider.maximumValue = _audioPlayer.duration;
+            [_slider addTarget:self action:@selector(sliderChanged:) forControlEvents:UIControlEventValueChanged];
+            [self addSubview:_slider];
+            
+            _bubbleView = [[UIView alloc] initWithFrame:CGRectMake(160, _slider.frame.origin.y - 46 + _slider.frame.size.height / 2, 72, 46)];
+            _bubbleView.backgroundColor = [UIColor clearColor];
+            _bubbleView.frame = [self createCurrentPositionFrame];
+            UIImageView *bubbleImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"player_bubble"]];
+            bubbleImageView.contentMode = UIViewContentModeScaleToFill;
+            bubbleImageView.frame = _bubbleView.bounds;
+            [_bubbleView addSubview:bubbleImageView];
+            _timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 10, 72, 11)];
+            _timeLabel.backgroundColor = [UIColor clearColor];
+            _timeLabel.textAlignment = NSTextAlignmentCenter;
+            _timeLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:10];
+            _timeLabel.text = [self calculateCurrentDuration];
+            [_bubbleView addSubview:_timeLabel];
+            
+            _bubbleView.hidden = YES;
+            [self addSubview:_bubbleView];
+            
+            self.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
+        }
+        
         return self;
     }
     
