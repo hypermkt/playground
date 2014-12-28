a735552d69426906239dc1e89212d0eebfbf46fb
Makoto Chiba <hypermkt@gmail.com>
2014-12-28 19:59:50 +0900

    エラー起きるけどとりあえず登録

--- iOS/SplitDataSourceFromTableView/SplitDataSourceFromTableView/ViewController.swift
+++ iOS/SplitDataSourceFromTableView/SplitDataSourceFromTableView/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController,
-    UITableViewDataSource,
+//    UITableViewDataSource,
     UITableViewDelegate
 {
     
@@ -23,8 +23,13 @@ class ViewController: UIViewController,
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+//        var dataSource = TableViewDataSource()
+//        dataSource.cities = self.cities
+        self.tableView.dataSource = TableViewDataSource() as UITableViewDataSource
+//        self.tableView.dataSource = dataSource
         
-        self.tableView.dataSource = self
+//        self.tableView.dataSource = self
         self.tableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -34,14 +39,14 @@ class ViewController: UIViewController,
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
-        
-        cell.textLabel.text = self.cities[indexPath.row]
-        return cell
-    }
-
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cities.count
-    }
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+//        
+//        cell.textLabel.text = self.cities[indexPath.row]
+//        return cell
+//    }
+//
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return cities.count
+//    }
 }
