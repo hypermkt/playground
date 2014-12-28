d8b706b040591fa145ac046833dcb23025684373
Makoto Chiba <hypermkt@gmail.com>
2014-12-28 19:34:02 +0900

    分割前の基礎を作成

---
+++ iOS/SplitDataSourceFromTableView/SplitDataSourceFromTableView/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  SplitDataSourceFromTableView
+//
+//  Created by 千葉 誠 on 2014/12/28.
+//  Copyright (c) 2014年 千葉 誠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,
+    UITableViewDataSource,
+    UITableViewDelegate
+{
+    
+    let cities = [
+        "入間市",
+        "狭山市",
+        "飯能市",
+    ]
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+        
+        cell.textLabel.text = self.cities[indexPath.row]
+        return cell
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+}
