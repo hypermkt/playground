e9ac7d6859aa6bbc75a514590abf963c37393d7a
Makoto Chiba <hypermkt@gmail.com>
2015-03-28 16:57:55 +0900

    threadSampleを追加

---
+++ iOS/threadSample/threadSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  threadSample
+//
+//  Created by 千葉 誠 on 2015/03/28.
+//  Copyright (c) 2015年 千葉 誠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
