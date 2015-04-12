c09316e4fad082cc058e315ad33c5b540b32d643
Makoto Chiba <hypermkt@gmail.com>
2015-04-12 15:24:53 +0900

    初期登録

---
+++ iOS/chat/chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chat
+//
+//  Created by 千葉 誠 on 2015/04/12.
+//  Copyright (c) 2015年 千葉 誠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
