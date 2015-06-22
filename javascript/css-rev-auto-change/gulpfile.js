5c7c46649d44bf079854aa48b316c42bcf105920
Makoto Chiba <hypermkt@gmail.com>
2015-06-22 23:40:49 +0900

    途中だけどコミット

--- javascript/css-rev-auto-change/gulpfile.js
+++ javascript/css-rev-auto-change/gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rev = require('gulp-rev');
+var revReplace = require('gulp-rev-replace');
 
 gulp.task('css', function() {
   gulp.src('src/*.css')
      .pipe(concat('all.css'))
      .pipe(rev())
+     .pipe(rev.manifest())
      .pipe(gulp.dest('dest/'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/*.css', ['css'])
+gulp.task("revreplace", ["css"], function(){
+  var manifest = gulp.src("./dest/rev-manifest.json");
+
+  return gulp.src("src/index.html")
+    .pipe(revReplace({manifest: manifest}))
+    .pipe(gulp.dest("./"));
 });
 
-gulp.task('default', ['css', 'watch']);
+gulp.task('watch', function() {
+  gulp.watch('src/*.css', ['css', 'revreplace'])
+});
+
+gulp.task('default', ['css', 'revreplace', 'watch']);
