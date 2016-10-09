75fa0c90c55cddff606bb3bdcb6df9335874d6ad
Makoto Chiba <hypermkt@paperboy.co.jp>
2016-10-10 00:52:33 +0900

    簡易E2Eテストコードを追加

---
+++ ruby/e2etest/spec/spec_helper.rb
@@ -0,0 +1,27 @@
+require 'capybara'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+RSpec.configure do |config|
+  # Capybaraを有効にする
+  config.include Capybara::DSL
+end
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(
+    app,
+    # SSLエラーを無視する
+    phantomjs_options: ['--ignore-ssl-errors=yes'],
+    debug: false,
+    :timeout => 300
+  )
+end
+
+# ブラウザなしにJavaScriptを実行する環境はpoltergeistを使う
+Capybara.javascript_driver = :poltergeist
+
+Capybara.configure do |config|
+  config.run_server = false
+  config.default_driver = :poltergeist
+  config.app_host = 'https://goope.jp/'
+end
