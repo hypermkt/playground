42755c701bd1aff3ea9795e552af1ffd0f9dcb14
Makoto Chiba <hypermkt@paperboy.co.jp>
2016-10-16 11:31:37 +0900

    バインディングのソースコードを追加

---
+++ javascript/phpcon2016/binding/main.js
@@ -0,0 +1,9 @@
+// main.js
+new Vue({
+  // 対象エレメントを指定します <div id="app">この中がVue.jsの対象範囲となる</div>
+  el: '#app',
+  // Vue インスタンスのためのデータオブジェクト
+  data: {
+    message: 'Hello PHPカンファレンス2016！'
+  }
+});
