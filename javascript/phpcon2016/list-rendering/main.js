9429c1315606ba7a282f6de819e3a79492bc83de
Makoto Chiba <hypermkt@gmail.com>
2016-10-16 21:15:49 +0900

    リストレンダリングのサンプルを追加

---
+++ javascript/phpcon2016/list-rendering/main.js
@@ -0,0 +1,12 @@
+// main.js
+new Vue({
+  el: '#app',
+  data: {
+    rankings: [
+      { title: 'Hoge' },
+      { title: 'Fuga' },
+      { title: 'Bar' },
+      { title: 'Foo' }
+    ]
+  }
+});
