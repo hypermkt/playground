ce92de49b09d450f003b56db36e9c0a703691125
Makoto Chiba <hypermkt@gmail.com>
2016-10-16 22:48:31 +0900

    メソッドとイベントレンダリングのサンプルを追加

---
+++ javascript/phpcon2016/method-and-event-handling/main.js
@@ -0,0 +1,20 @@
+// main.js
+new Vue({
+  el: '#app',
+  data: {
+    message: '∥熱湯風呂∥'
+  },
+  // ライフサイクルフック：created
+  // インスタンスが作成された後に呼ばれる
+  created: function() {
+    setTimeout(function() {
+      alert('押せよー！'); 
+    }, 5000);
+  },
+  // Vue インスタンスに組み込まれるメソッド宣言箇所
+  methods: {
+    osu: function() {
+      this.message = '訴えてやる！';
+    }
+  }
+});
