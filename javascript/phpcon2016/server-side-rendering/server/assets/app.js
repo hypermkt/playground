72f6beb66b0c71d4c6aca90bcdc4ba105e25adf6
Makoto Chiba <hypermkt@paperboy.co.jp>
2016-10-31 21:47:27 +0900

    サーバーサイドレンダリングのみに変更

--- javascript/phpcon2016/server-side-rendering/server/assets/app.js
+++ javascript/phpcon2016/server-side-rendering/server/assets/app.js
@@ -1,28 +1,8 @@
-(function () { 'use strict'
-  var createApp = function () {
-    // ---------------------
-    // 通常のアプリケーションコード
-    // ---------------------
-
-    // クライアントサイドのコードが読み込み後にそれを引き継げるよう、
-    // id "app"をルートノードにもつメインの Vue インスタンスが
-    // 返却されなければなりません。
-    return new Vue({
-      template: '<div id="app">Helo PHPカンファレンス{{ year }}!</div>',
-      data: {
-        year: 2016
-      }
-    })
-
-    // -------------------
-    // 通常のアプリケーションコード 終わり
-    // -------------------
-  }
-  // SSR時はこちらを通る
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = createApp
-  // ブラウザアクセス時はこちらを通る
-  } else {
-    this.app = createApp()
-  }
-}).call(this)
+module.exports = function () {
+  return new Vue({
+    template: '<div id="app">Helo PHPカンファレンス{{ year }}!</div>',
+    data: {
+      year: 2016
+    }
+  })
+} 
