e50cde5771911097b31024bcc831dca27fdbf332
Makoto Chiba <hypermkt@paperboy.co.jp>
2016-11-01 22:47:28 +0900

    しょうもないネタはやめる

--- javascript/phpcon2016/method-and-event-handling/main.js
+++ javascript/phpcon2016/method-and-event-handling/main.js
@@ -2,19 +2,19 @@
 new Vue({
   el: '#app',
   data: {
-    message: '∥熱湯風呂∥'
+    message: '好きな言語は何ですか？'
   },
   // ライフサイクルフック：created
   // インスタンスが作成された後に呼ばれる
   created: function() {
     setTimeout(function() {
-      alert('押せよー！'); 
+      alert('早く答えてください！'); 
     }, 5000);
   },
   // Vue インスタンスに組み込まれるメソッド宣言箇所
   methods: {
-    push: function() {
-      this.message = '訴えてやる！';
+    answer: function() {
+      this.message = 'PHP!';
     }
   }
 });
