0a72fda92819ef1fa624be09305e7bfec87fd71f
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-06-20 20:39:03 +0900

    Add avoriaz test sample.

--- javascript/vuejs-avoriaz002/test/unit/Hello.spec.js
+++ javascript/vuejs-avoriaz002/test/unit/Hello.spec.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Hello from '../../src/components/Hello.vue'
+import { mount } from 'avoriaz'
 
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
@@ -8,6 +9,11 @@ describe('Hello.vue', () => {
       render: (h) => h(Hello)
     })
     expect(vm.$el.querySelector('h1').textContent).to.be.eql('Welcome to Your Vue.js App')
+  })
+
+  it('avoriaz: should render correct contents', () => {
+    const wrapper = mount(Hello)
+    expect(wrapper.find('h1')[0].text()).to.be.eql('Welcome to Your Vue.js App')
   })
 })
 
