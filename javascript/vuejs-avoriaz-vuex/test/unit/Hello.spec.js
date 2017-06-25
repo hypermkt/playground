30d73ed81183f141d31ca084b52bcee233f5ab1e
Makoto Chiba <hypermkt@gmail.com>
2017-06-26 02:59:18 +0900

    Add module test

--- javascript/vuejs-avoriaz-vuex/test/unit/Hello.spec.js
+++ javascript/vuejs-avoriaz-vuex/test/unit/Hello.spec.js
@@ -4,6 +4,7 @@ import { mount } from 'avoriaz'
 import sinon from 'sinon'
 import 'babel-polyfill' // Error: [vuex] vuex requires a Promise polyfill in this browser.対策
 import Hello from '../../src/components/Hello.vue'
+import counter from '../../src/store/modules/counter'
 
 Vue.use(Vuex)
 
@@ -63,6 +64,36 @@ describe('Hello.vue', () => {
       const wrapper = mount(Hello, { store })
       const h3 = wrapper.find('h3')[0]
       expect(h3.text()).to.be.eql('Count: 0')
+    })
+  })
+
+  describe('Mocking with Modules', () => {
+    let actions;
+    let state;
+    let store;
+
+    beforeEach(() => {
+      state = {
+        counter: {
+          count: 0
+        }
+      }
+      actions = {
+        increment: sinon.stub(),
+        input_number: sinon.stub(),
+      }
+      store = new Vuex.Store({
+        state,
+        actions,
+        getters: counter.getters
+      })
+    })
+
+    it('モジュールを利用した場合', () => {
+      const wrapper = mount(Hello, { store })
+      const button = wrapper.find('button')[0]
+      button.trigger('click')
+      expect(actions.increment.calledOnce).to.be.eql(true)
     })
   })
 })
