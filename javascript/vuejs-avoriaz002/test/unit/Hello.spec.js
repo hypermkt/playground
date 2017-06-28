792457bcd9405e5d5e8c7b4cb0dafc233d523399
Makoto Chiba <hypermkt@gmail.com>
2017-06-29 00:51:18 +0900

    refactor

--- javascript/vuejs-avoriaz002/test/unit/Hello.spec.js
+++ javascript/vuejs-avoriaz002/test/unit/Hello.spec.js
@@ -2,24 +2,24 @@ import Hello from '../../src/components/Hello.vue'
 import { mount, shallow } from 'avoriaz'
 
 describe('avoriaz test', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(Hello, {
+      propsData: {
+        user: {nickname: 'John'}
+      }
+    })
+  })
+
   describe('mount', () => {
     it('contains', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       expect(wrapper.contains('img')).to.be.eql(true)
       expect(wrapper.contains('h1')).to.be.eql(true)
       expect(wrapper.contains('h2')).to.be.eql(true)
     })
 
     it('data', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       let msg = 'Hello World'
       wrapper.setData({msg: msg})
       expect(wrapper.find('h1')[0].text()).to.be.eql(msg)
@@ -27,77 +27,37 @@ describe('avoriaz test', () => {
     })
 
     it('find', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       expect(wrapper.find('h2').length).to.be.eql(2)
       expect(wrapper.find('h2')[0].is('h2')).to.be.eql(true)
     })
 
     it('hasAttribute', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       expect(wrapper.hasAttribute('id', 'hello')).to.be.eql(true)
     })
 
     it('instance', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       expect(wrapper.instance().hello()).to.be.eql('world')
       expect(wrapper.instance().msg).to.be.eql('Welcome to Your Vue.js App')
     })
 
     it('isVueComponent', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       expect(wrapper.isVueComponent).to.be.eql(true)
     })
 
     it('methods', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       expect(wrapper.methods().hello()).to.be.eql('world')
     })
 
     it('name', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       expect(wrapper.name()).to.be.eql('hello')
     })
 
     it('is', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       // ref: https://eddyerburgh.gitbooks.io/avoriaz/content/api/mount/is.html
       expect(wrapper.find('div#hello h2')[0].is('h2')).to.be.eql(true)
     })
 
     it('propsData', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       expect(wrapper.find('h3')[0].text()).to.be.eql('nickname: John')
 
       // propsDataを使用すると「warning: functions returned by propsData() will not have this bound to the vue instance. Calling a propsData function that uses this will result in an error. You can access propsData functions by using the vue instance. e.g. to call a method function named propsDataFunc, call wrapper.vm.$props.propsDataFunc(). See https://github.com/eddyerburgh/avoriaz/issues/15」が表示されたので、wrapper.vmに変更
@@ -108,41 +68,22 @@ describe('avoriaz test', () => {
     })
 
     it('text', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       expect(wrapper.find('h1')[0].text()).to.be.eql('Welcome to Your Vue.js App')
     })
 
     it('vm', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
       expect(wrapper.vm.user.nickname).to.be.eql('John')
     })
   })
 
   describe('shallow', () => {
-    const wrapper = shallow(Hello, {
-      propsData: {
-        user: {nickname: 'John'}
-      }
+    it('isVueComponent', () => {
+      expect(wrapper.isVueComponent).to.be.eql(true)
     })
-    expect(wrapper.isVueComponent).to.be.eql(true)
   })
 
   describe('selectors', () => {
     it('tag selectors', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
-
       // タグを指定する
       const div = wrapper.find('div')[0]
       expect(div.is('div')).to.be.eql(true)
@@ -157,34 +98,16 @@ describe('avoriaz test', () => {
     })
 
     it('id selectors', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
-
       const hello = wrapper.find('#hello')[0]
       expect(hello.is('div')).to.be.eql(true)
     })
 
     it('class selectors', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
-
       const subtitle = wrapper.find('.subtitle')[0]
       expect(subtitle.is('h2')).to.be.eql(true)
     })
 
     it('attribute selectors', () => {
-      const wrapper = mount(Hello, {
-        propsData: {
-          user: {nickname: 'John'}
-        }
-      })
-
       const src1 = wrapper.find('[src]')[0]
       expect(src1.is('img')).to.be.eql(true)
 
