97ae1415d1e2ad986bc273a0b3433f03cde6a718
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-08-10 12:35:18 +0900

    Add async component

--- javascript/vue/async_components/src/App.vue
+++ javascript/vue/async_components/src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
+    <async-example></async-example>
     <hello></hello>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import Hello from './components/Hello.vue'
+
+Vue.component('async-example', function (resolve, reject) {
+  setTimeout(function () {
+    // Pass the component definition to the resolve callback
+    resolve({
+      template: '<div>I am async!</div>'
+    })
+  }, 3000)
+})
 
 export default {
   name: 'app',
