085e74215cdcab27f8e5ef8e1ac1ba2441c5d16f
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-08-12 12:04:32 +0900

    Add vue-select sample

---
+++ javascript/vue-select/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h2>Multi Selection</h2>
+    selected: {{ selected }}
+    <button @click="consoleLog()">console.log(JSON.stringify(this.selected))</button>
+    <p><v-select multiple v-model="selected" :options="options"></v-select></p>
+  </div>
+</template>
+
+<script>
+import vSelect from "vue-select"
+export default {
+  components: {vSelect},
+
+  data() {
+    return {
+      selected: null,
+      options: ['foo','bar','baz']
+    }
+  },
+  watch: {
+    selected() {
+      console.log(JSON.stringify(this.selected));
+    }
+  },
+  methods: {
+    consoleLog: function() {
+      console.log(JSON.stringify(this.selected));
+    }
+  }
+}
+</script>
