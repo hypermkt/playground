a2b95dcaefc57fa01b92857784101e402c4a33bc
Makoto Chiba <hypermkt@gmail.com>
2018-02-11 21:59:55 +0900

    PHPにPECLでMemcachedをインストール

--- php/store-session-in-mysql/Dockerfile.php
+++ php/store-session-in-mysql/Dockerfile.php
@@ -1,4 +1,6 @@
 FROM php:7.2-apache
 
-RUN apt-get update && apt-get -qq install -y --no-install-recommends vim tree
+RUN apt-get update && apt-get -qq install -y --no-install-recommends vim tree zlib1g-dev libmemcached-dev
 RUN docker-php-ext-install pdo_mysql
+RUN pecl install memcached
+RUN docker-php-ext-enable memcached
