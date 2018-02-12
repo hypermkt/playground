532bf5e4954b078554a8e87a5efe293dae146903
Makoto Chiba <hypermkt@gmail.com>
2018-02-12 21:16:08 +0900

    セッション登録の検証用に変更

--- php/store-session-in-mysql/index.php
+++ php/store-session-in-mysql/index.php
@@ -1,8 +1,7 @@
 <?php
 
-try {
-  $pdo = new PDO('mysql:host=database;dbname=development;charset=utf8','development','password',
-    array(PDO::ATTR_EMULATE_PREPARES => false));
-} catch (PDOException $e) {
-  exit('データベース接続失敗。'.$e->getMessage());
-}
+session_start();
+
+$_SESSION['hoge'] = 'fuga';
+
+var_dump($_SESSION['hoge']);
