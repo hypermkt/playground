d269254a2eb7e043f9c61f5e063bd194f67d58ef
Makoto Chiba <hypermkt@paperboy.co.jp>
2018-03-28 15:30:34 +0900

    初期データの登録用シーダーを作成

--- php/laravel-api-sample/database/seeds/DatabaseSeeder.php
+++ php/laravel-api-sample/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = \Faker\Factory::create();
+
+        $user = new \App\User();
+        $user->name = $faker->name;
+        $user->email = $faker->unique()->safeEmail;
+        $user->password = bcrypt('password');
+        $user->remember_token = str_random(10);
+        $user->save();
+
+        $item = new \App\Item();
+        $item->user_id = $user->id;
+        $item->content = $faker->text();
+        $item->save();
     }
 }
