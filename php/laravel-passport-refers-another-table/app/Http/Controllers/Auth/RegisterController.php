278fd1b7bfc96e5b6d14376ea1ddaf9ea30f2ea7
Makoto Chiba <hypermkt@paperboy.co.jp>
2018-05-07 12:28:51 +0900

    ユーザー登録完了時の登録先テーブルを変更

--- php/laravel-passport-refers-another-table/app/Http/Controllers/Auth/RegisterController.php
+++ php/laravel-passport-refers-another-table/app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -63,7 +63,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        return Customer::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
