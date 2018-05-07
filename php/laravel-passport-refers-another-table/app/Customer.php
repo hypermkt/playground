5a8424fd3a54bfc6dbdd2d86224137eb4da2cee4
Makoto Chiba <hypermkt@paperboy.co.jp>
2018-05-07 14:52:51 +0900

    login_iでユーザー登録できるようにする

--- php/laravel-passport-refers-another-table/app/Customer.php
+++ php/laravel-passport-refers-another-table/app/Customer.php
@@ -16,7 +16,7 @@ class Customer extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'login_id', 'password',
     ];
 
     /**
--- php/laravel-passport-refers-another-table/app/Http/Controllers/Auth/RegisterController.php
+++ php/laravel-passport-refers-another-table/app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'login_id' => 'required|string|max:255',
+//            'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
@@ -65,7 +66,8 @@ class RegisterController extends Controller
     {
         return Customer::create([
             'name' => $data['name'],
-            'email' => $data['email'],
+            'login_id' => $data['login_id'],
+//            'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
     }
