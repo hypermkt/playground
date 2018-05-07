618b04df05b3bec9e8fdce90f06eec42ccc09e9f
Makoto Chiba <hypermkt@paperboy.co.jp>
2018-05-07 12:24:54 +0900

    Customerを認証用テーブル用に調整

--- php/laravel-passport-refers-another-table/app/Customer.php
+++ php/laravel-passport-refers-another-table/app/Customer.php
@@ -3,8 +3,28 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Customer extends Model
+class Customer extends Authenticatable
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
