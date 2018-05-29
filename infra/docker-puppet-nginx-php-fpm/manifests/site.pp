a6b340e35789e73545dea2c4f270b88a480e9d80
Makoto Chiba <hypermkt@gmail.com>
2018-05-29 09:15:20 +0900

    Nginxのconfファイルをfileリソースで配置したいがうまくいかない

--- infra/docker-puppet-nginx-php-fpm/manifests/site.pp
+++ infra/docker-puppet-nginx-php-fpm/manifests/site.pp
@@ -1,9 +1,19 @@
 include ::nginx
 
-nginx::resource::server { 'hypermkt.local':
-  www_root => '/var/www/hypermkt/public',
-  ensure => present, # 常に起動している状態にする
-  require  => [Package['nginx'], Class['Nginx::Config']],
+#nginx::resource::server { 'hypermkt.local':
+#  www_root => '/var/www/hypermkt/public',
+#  ensure => present, # 常に起動している状態にする
+#  require  => [Package['nginx'], Class['Nginx::Config']],
+#}
+
+file { '/etc/nginx/conf.d/default.conf':
+  ensure => present,
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
+  content => template('nginx/default.conf'),
+  require => Package['nginx'],
+  notify => Service['nginx'],
 }
 
 $dirs = ['/var/www', '/var/www/hypermkt/', '/var/www/hypermkt/public']
