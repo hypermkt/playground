55dd6d3c482a7db6e214c7095fc022393d555929
Makoto Chiba <hypermkt@paperboy.co.jp>
2018-06-10 22:58:49 +0900

    認証をするためにAuthenticatableの継承が必要だった

--- php/laravel-passport-change-auth/app/Account.php
+++ php/laravel-passport-change-auth/app/Account.php
@@ -4,8 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Account extends Model
+class Account extends Authenticatable
 {
     use HasApiTokens;
 
