d5cbe748cd67a09354df59350a4f0c429983e137
Makoto Chiba <hypermkt@paperboy.co.jp>
2018-06-10 22:30:48 +0900

    Userモデル同様に Authenticatable を継承する

--- php/laravel-passport-change-auth/app/Account.php
+++ php/laravel-passport-change-auth/app/Account.php
@@ -3,8 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Account extends Model
+class Account extends Authenticatable
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'login_id', 'pass',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'pass'
+    ];
 }
