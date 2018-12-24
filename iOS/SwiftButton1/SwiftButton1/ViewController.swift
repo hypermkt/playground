2ccd5a2f9dc5fee10b93b0216424bf3dacfe293a
Makoto Chiba <hypermkt@gmail.com>
2018-12-24 22:21:57 +0900

    Add SwiftButton Sample

---
+++ iOS/SwiftButton1/SwiftButton1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SwiftButton1
+//
+//  Created by hypermkt on 2018/12/24.
+//  Copyright © 2018 hypermkt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var count = 5
+
+    // 画面が表示される時にた最初に呼ばれる
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        count = 10
+        
+        print(count)
+    }
+    
+    func hoge()
+    {
+        
+    }
+
+
+}
+
