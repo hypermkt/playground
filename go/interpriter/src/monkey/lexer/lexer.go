770d21d7c09806a4c7a220bdeebfcff9df595f23
Makoto Chiba <hypermkt@paperboy.co.jp>
2019-02-06 13:56:31 +0900

    Add Lexer

--- go/interpriter/src/monkey/lexer/lexer.go
+++ go/interpriter/src/monkey/lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "monkey/token"
+
 type Lexer struct {
 	input        string
 	position     int  // 入力における現在の位置(現在の文字を指し示す)
@@ -9,11 +11,12 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
-	l.readChara()
+	l.readChar()
 	return l
 }
 
-func (l *Lexer) readChara() {
+// readChar 1文字読んで、input文字列の現在位置を進める
+func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
@@ -21,4 +24,38 @@ func (l *Lexer) readChara() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+// NextToken 次のトークンを取得する
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
--- go/interpriter/src/monkey/lexer/lexer_test.go
+++ go/interpriter/src/monkey/lexer/lexer_test.go
@@ -33,7 +33,7 @@ func TestNextToken(t *testing.T) {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
 		}
 
-		if tok.expectedLiteral != tt.expectedLiteral {
+		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
 		}
 	}
