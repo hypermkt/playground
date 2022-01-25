5cffd9d3534d28b4ce6257b2711d3a6cd6ae3414
hypermkt <hypermkt@gmail.com>
2022-01-25 21:30:02 +0900

    ヘッダー部の見た目調整

--- react/wordle-clone/src/components/Header/Header.js
+++ react/wordle-clone/src/components/Header/Header.js
@@ -1,17 +1,33 @@
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Heading, Icon, Flex, Spacer } from '@chakra-ui/react'
+import { FaRegQuestionCircle } from "react-icons/fa";
+import { IoSettingsSharp, IoStatsChart } from "react-icons/io5";
+
 
 export const Header = () => {
   return (
     <>
       <Box
         w="500px"
-        p={4}
+        p={1}
         borderBottom="1px"
         borderBottomColor="#d3d6da"
         color="black"
         align="center"
       >
-        <Heading>WORDLE</Heading>
+        <Flex>
+          <Box p="2" width="80px">
+            <Icon m="1" w={6} h={6} as={FaRegQuestionCircle} color="#999999" />
+          </Box>
+          <Spacer />
+          <Box >
+            <Heading>WORDLE</Heading>
+          </Box>
+          <Spacer />
+          <Box p="2" width="80px">
+            <Icon m="1" w={6} h={6} as={IoStatsChart} color="#999999" />
+            <Icon m="1" w={6} h={6} as={IoSettingsSharp} color="#999999" />
+          </Box>
+        </Flex>
       </Box>
     </>
   );
