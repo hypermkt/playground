e18d171e29d636df4a538dbd1294e17c6b9b1f52
hypermkt <hypermkt@gmail.com>
2022-01-27 11:50:44 +0900

    文字列は5個まで入力、文字列削除を実装

--- react/wordle-clone/src/redux/reducer/woodle.reducer.js
+++ react/wordle-clone/src/redux/reducer/woodle.reducer.js
@@ -5,9 +5,16 @@ const initialState = {
 export const wordle = (state = initialState, action)  => {
   switch(action.type) {
     case 'INPUT_WORD':
+      const newWords = (state.words.length <= 4) ? state.words.concat(action.payload) : state.words;
       return {
         ...state,
-        words: state.words.concat(action.payload),
+        words: newWords,
+      }
+    case 'DELETE_WORD':
+      let deletedWords = state.words.slice(0, state.words.length - 1)
+      return {
+        ...state,
+        words: deletedWords
       }
 
     default:
