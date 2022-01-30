70c98c272ed37939c6daea21823a4f75855a9095
hypermkt <hypermkt@gmail.com>
2022-01-30 23:16:12 +0900

    ボタンの色種類を設定

--- react/wordle-clone/src/components/Board/Board.js
+++ react/wordle-clone/src/components/Board/Board.js
@@ -1,11 +1,42 @@
 import { Square, Text } from '@chakra-ui/react'
+import styled, { css } from "styled-components"
 
 export const Board = (props) => {
   return (
     <>
-      <Square size="90px" border="2px" borderColor="#d3d6da">
+      <WrappedSqure $mode={props.mode} size="90px" border="2px" borderColor="#d3d6da">
         <Text fontSize="6xl">{props.character}</Text>
-      </Square>
+      </WrappedSqure>
     </>
   )
 }
+
+const WrappedSqure = styled(Square)`
+  ${(props) => {
+    switch (props.$mode) {
+      case "normal":
+        return css`
+          color: black;
+          background: white;
+        `;
+
+      case "wrong":
+        return css`
+          color: white;
+          background: #a9a9a9;
+        `;
+
+      case "currect":
+        return css`
+          color: white;
+          background: #538d4e;
+        `;
+
+      case "near":
+        return css`
+          color: white;
+          background: #c9b458;
+        `;
+    }
+  }}
+`;
--- react/wordle-clone/src/components/BoardContainer/BoardContainer.js
+++ react/wordle-clone/src/components/BoardContainer/BoardContainer.js
@@ -14,35 +14,35 @@ export const BoardContainer = () => {
     <>
       <Box w="100%" p={4}>
         <SimpleGrid columns={5} spacing={3}>
-          <Board character={words1[0]} />
-          <Board character={words1[1]} />
-          <Board character={words1[2]} />
-          <Board character={words1[3]} />
-          <Board character={words1[4]} />
+          <Board character={words1[0]} mode={'normal'} />
+          <Board character={words1[1]} mode={'wrong'} />
+          <Board character={words1[2]} mode={'currect'} />
+          <Board character={words1[3]} mode={'near'} />
+          <Board character={words1[4]} mode={'normal'} />
 
-          <Board character={words2[0]} />
-          <Board character={words2[1]} />
-          <Board character={words2[2]} />
-          <Board character={words2[3]} />
-          <Board character={words2[4]} />
+          <Board character={words2[0]} mode={'normal'} />
+          <Board character={words2[1]} mode={'normal'} />
+          <Board character={words2[2]} mode={'normal'} />
+          <Board character={words2[3]} mode={'normal'} />
+          <Board character={words2[4]} mode={'normal'} />
 
-          <Board character={words3[0]} />
-          <Board character={words3[1]} />
-          <Board character={words3[2]} />
-          <Board character={words3[3]} />
-          <Board character={words3[4]} />
+          <Board character={words3[0]} mode={'normal'} />
+          <Board character={words3[1]} mode={'normal'} />
+          <Board character={words3[2]} mode={'normal'} />
+          <Board character={words3[3]} mode={'normal'} />
+          <Board character={words3[4]} mode={'normal'} />
 
-          <Board character={words4[0]} />
-          <Board character={words4[1]} />
-          <Board character={words4[2]} />
-          <Board character={words4[3]} />
-          <Board character={words4[4]} />
+          <Board character={words4[0]} mode={'normal'} />
+          <Board character={words4[1]} mode={'normal'} />
+          <Board character={words4[2]} mode={'normal'} />
+          <Board character={words4[3]} mode={'normal'} />
+          <Board character={words4[4]} mode={'normal'} />
 
-          <Board character={words5[0]} />
-          <Board character={words5[1]} />
-          <Board character={words5[2]} />
-          <Board character={words5[3]} />
-          <Board character={words5[4]} />
+          <Board character={words5[0]} mode={'normal'} />
+          <Board character={words5[1]} mode={'normal'} />
+          <Board character={words5[2]} mode={'normal'} />
+          <Board character={words5[3]} mode={'normal'} />
+          <Board character={words5[4]} mode={'normal'} />
         </SimpleGrid>
       </Box>
     </>
